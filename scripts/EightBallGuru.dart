050f59f546a7fcf7dcb94eff034b95c4f26ee021
Mark Dewey <mark.t.dewey@gmail.com>
2012-09-23 16:56:28 -0400

    Added init logic

--- scripts/EightBallGuru.dart
+++ scripts/EightBallGuru.dart
@@ -1,19 +1,87 @@
 
 #import('dart:html');
+#import('dart:math');
 
-num rotatePos = 0;
 
+//page load
 void main() {
-  query("#text").text = "Click me!";
-
-  query("#text").on.click.add(rotateText);
+  //adds handlers to buttons
+  initButtons();
+  
+}
+/*
+ * Business Logic
+ * 
+ * */
+void addItemToList()
+{
+  //get value from textbox
+  InputElement box = query('#newItemBox');
+    
+  //add item to list
+  String newthing = box.value;
+   
+  //TODO: format list item better
+  var newItem = new Element.html("<span class='item'><b> $newthing </b></span>");
+  
+  var list = query("#List");
+  var br = new Element.html("<br/>");
+  
+  list.nodes.add(newItem);
+  list.nodes.add(br);
+  
+  //clear input box
+  box.value="";
 }
 
-void rotateText(Event event) {
-  rotatePos += 360;
+void chooseItem()
+{
+  //gather all items in an array from the DOM
+  var itemListFromDOM = queryAll(".item");
+  var itemList = new Map();
+  var count = 0;
+  for (var item in itemListFromDOM)
+  {
+    itemList[count] = item.nodes[0].text;
+    count++;
+  }
+  for (var i = 0; i < count; i ++)
+  {
+    print(itemList[i]);
+  }
 
-  var textElement = query("#text");
+  //pick one
+  var choice = getChoice(count);
+  var selectedItem = itemList[choice];
+  window.alert("Selected item: $selectedItem ");
+  //TODO:clear list
+  
+  
+  //TODO:add this item to list as selected
+  
+}
 
-  textElement.style.transition = "1s";
-  textElement.style.transform = "rotate(${rotatePos}deg)";
+
+int getChoice(max)
+{
+  var ch;
+  var r = new Random();
+  ch = r.nextInt(max+1) ;
+  return ch;
+}
+
+
+/*
+ * Boilerplate functions ??
+ * Subject to change as more projects are made
+ * */
+void initButtons()
+{
+  //add item to screen button
+   var addButton = document.query('#addButton');
+   addButton.on.click.add((event) => addItemToList() );
+   
+   //pick item
+   var chooseButton = document.query('#chooseButton');
+   chooseButton.on.click.add((event) => chooseItem() );
 }
